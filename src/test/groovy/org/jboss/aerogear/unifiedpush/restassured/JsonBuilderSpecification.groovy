7fcddb15710db35023c6018cd243fabc975f19d7
tolis-e <tolisemm@yahoo.gr>
2013-08-12 16:19:59 +0200

    Update itests & added AuthenticationEndpoint tests

---
+++ src/test/groovy/org/jboss/aerogear/unifiedpush/restassured/JsonBuilderSpecification.groovy
@@ -0,0 +1,60 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.unifiedpush.restassured
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import spock.lang.Shared
+import spock.lang.Specification
+
+class JsonBuilderSpecification extends ParentSpecification {
+
+    def "JSON map and non-map syntax"() {
+        given: "Two JSON builders"
+
+        when: "Creating a JSON using map and non-map syntax"
+        def string1 = JsonOutput.toJson(json {
+            foo: "bar"
+            name: "namebar"
+        })
+
+        def string2 = JsonOutput.toJson(json {
+            foo "bar"
+            name "namebar"
+        })
+
+        def string3 = JsonOutput.toJson([foo:"bar", name:"namebar"])
+
+        then: "Map syntax yields empty JSON object"
+        string1 == "{}"
+
+        and: "Non-map syntax yields correct JSON object"
+        string2 == '{"foo":"bar","name":"namebar"}'
+
+        and: "Non JSON Map syntax yields correct JSON object"
+        string3 == string2
+    }
+}
+
+abstract class ParentSpecification extends Specification {
+
+    def Closure json = new Closure(this, this) {
+        def doCall(Object args) {
+            new JsonBuilder().call(args)
+        }
+    }
+}
