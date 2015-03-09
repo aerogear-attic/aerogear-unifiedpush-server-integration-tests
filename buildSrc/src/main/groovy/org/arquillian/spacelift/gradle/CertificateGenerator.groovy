8287173818e57ee69d9350079a21a4b1c7e0304a
Tadeas Kriz <tadeas.kriz@gmail.com>
2015-03-09 16:11:06 +0100

    Update tests to run against openshift with proper certificates.

---
+++ buildSrc/src/main/groovy/org/arquillian/spacelift/gradle/CertificateGenerator.groovy
@@ -0,0 +1,136 @@
+package org.arquillian.spacelift.gradle
+
+import org.arquillian.spacelift.Spacelift
+import org.arquillian.spacelift.task.Task
+import org.arquillian.spacelift.task.os.CommandTool
+
+class CertificateGenerator extends Task<Object, Void> {
+
+    private String commonName = 'localhost'
+    private String alias = 'aerogear'
+    private String password = 'aerogear'
+    private File trustStore
+    private File apnsCertificate
+    private File gcmCertificate
+
+    String commonName() {
+        return commonName
+    }
+
+    CertificateGenerator commonName(String commonName) {
+        this.commonName = commonName
+        return this
+    }
+
+    String alias() {
+        return alias
+    }
+
+    CertificateGenerator alias(String alias) {
+        this.alias = alias
+        return this
+    }
+
+    String password() {
+        return password
+    }
+
+    CertificateGenerator password(String password) {
+        this.password = password
+        return this
+    }
+
+    File trustStore() {
+        return trustStore
+    }
+
+    CertificateGenerator trustStore(String trustStorePath) {
+        return trustStore(new File(trustStorePath))
+    }
+
+    CertificateGenerator trustStore(File trustStore) {
+        this.trustStore = trustStore
+        return this
+    }
+
+    File apnsCertificate() {
+        return apnsCertificate
+    }
+
+    CertificateGenerator apnsCertificate(String apnsCertificatePath) {
+        return apnsCertificate(new File(apnsCertificatePath))
+    }
+
+    CertificateGenerator apnsCertificate(File apnsCertificate) {
+        this.apnsCertificate = apnsCertificate
+        return this
+    }
+
+    File gcmCertificate() {
+        return gcmCertificate
+    }
+
+    CertificateGenerator gcmCertificate(String gcmCertificatePath) {
+        return gcmCertificate(new File(gcmCertificatePath))
+    }
+
+    CertificateGenerator gcmCertificate(File gcmCertificate) {
+        this.gcmCertificate = gcmCertificate
+        return this
+    }
+
+
+    @Override
+    protected Void process(Object input) throws Exception {
+        createApnsCertificate()
+
+        importCertificatesIntoTrustStore()
+
+        return null
+    }
+
+    private void createApnsCertificate() {
+        CommandTool keytool = Spacelift.task('keytool') as CommandTool
+
+        keytool.parameters('-genkey', '-noprompt')
+                .parameters('-alias', alias)
+                .parameters('-dname', "CN=$commonName, OU=UnifiedPush, O=AeroGear, C=US")
+                .parameters('-keystore', apnsCertificate.absolutePath)
+                .parameters('-storepass', password)
+                .parameters('-validity', '365')
+                .parameters('-keyalg', 'RSA')
+                .parameters('-keysize', '2048')
+                .parameters('-storetype', 'pkcs12')
+
+        keytool.execute().await()
+    }
+
+    private void importCertificatesIntoTrustStore() {
+        CommandTool keytool = Spacelift.task('keytool') as CommandTool
+
+        keytool.parameters('-importkeystore')
+//                .parameters('-v', '-trustcacerts')
+                .parameters('-alias', alias)
+                .parameters('-srckeystore', apnsCertificate.absolutePath)
+                .parameters('-srcstorepass', password)
+                .parameters('-srcstoretype', 'pkcs12')
+                .parameters('-destkeystore', trustStore.absolutePath)
+                .parameters('-deststoretype', 'jks')
+                .parameters('-storepass', password)
+                .parameters('-keypass', password)
+
+        keytool.execute().await()
+
+//        keytool -importkeystore -destkeystore mykeystore.jks -srckeystore keystore.p12 -srcstoretype pkcs12 -alias myservercert
+
+
+        /*
+        keytool -import -storepass "Be Your Own Lantern" -keystore /home/asaleh/Work/PROXY/JBOSS/jboss-eap-6.3/bin/littleproxy_keystore.jks -alias android -file /home/asaleh/Work/jbossqe-mobile/patches/certs/sslcerts/myHost.crt  -trustcacerts -keypass aerogear
+
+keytool -import -storepass aerogear -keystore /home/asaleh/Work/jbossqe-mobile/patches/certs/aerogear.keystore -alias android -file /home/asaleh/Work/jbossqe-mobile/patches/certs/sslcerts/myHost.crt -noprompt -trustcacerts -keypass aerogear
+
+keytool -import -v -trustcacerts -alias android -file /home/asaleh/Work/jbossqe-mobile/patches/certs/sslcerts/myHost.crt -keystore /home/asaleh/Work/jbossqe-mobile/patches/certs/aerogear.truststore -keypass aerogear -storepass aerogear
+         */
+    }
+
+}
--- buildSrc/src/main/groovy/org/arquillian/spacelift/gradle/openshift/UPSOpenShiftInstallation.groovy
+++ buildSrc/src/main/groovy/org/arquillian/spacelift/gradle/openshift/UPSOpenShiftInstallation.groovy
@@ -1,5 +1,8 @@
 package org.arquillian.spacelift.gradle.openshift
 
+import org.arquillian.spacelift.gradle.CertificateGenerator
+import org.arquillian.spacelift.gradle.keytool.KeyTool
+
 import java.io.File
 import java.lang.reflect.Array
 import java.util.concurrent.Callable
@@ -49,13 +52,13 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
     DeferredValue<String> openShiftAppName = DeferredValue.of(String.class).from("upsintegrationtests")
 
     DeferredValue<String> openShiftNamespace = DeferredValue.of(String.class).from("mobileqa")
-    
+
     DeferredValue<String> openShiftGearSize = DeferredValue.of(String.class).from("small")
 
     DeferredValue<String> openShiftUsername = DeferredValue.of(String.class)
 
     DeferredValue<String> openShiftPassword = DeferredValue.of(String.class)
-    
+
     DeferredValue<String> openShiftToken = DeferredValue.of(String.class)
 
     DeferredValue<List> openShiftCartridges = DeferredValue.of(List.class).from([ 'https://raw.githubusercontent.com/jboss-mobile/jboss-unified-push-openshift-cartridge/master/metadata/manifest.yml' ])
@@ -79,7 +82,11 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
     DeferredValue<Integer> openShiftHttpsProxyPort = DeferredValue.of(Integer.class).from(16000)
 
     DeferredValue<Integer> gcmMockServerPort = DeferredValue.of(Integer.class).from(16001)
-    
+
+    DeferredValue<File> aerogearGcmMockCertificate = DeferredValue.of(File.class)
+
+    DeferredValue<File> aerogearGcmMockKey = DeferredValue.of(File.class)
+
     DeferredValue<Integer> aerogearApnsPushPort = DeferredValue.of(Integer.class).from(16002)
 
     DeferredValue<Integer> aerogearApnsFeedbackPort = DeferredValue.of(Integer.class).from(16003)
@@ -87,6 +94,7 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
     DeferredValue<String> openShiftAppDirectory = DeferredValue.of(String.class).from("unified-push")
 
     DeferredValue<String> unifiedPushTestExtensionVersion = DeferredValue.of(String.class)
+
 
     // tools provided by this installation
     InheritanceAwareContainer<GradleTask, DefaultGradleTask> tools
@@ -121,6 +129,9 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
         this.openShiftAppDirectory = other.@openShiftAppDirectory.copy()
         this.unifiedPushTestExtensionVersion = other.@unifiedPushTestExtensionVersion.copy()
         this.tools = (InheritanceAwareContainer<GradleTask, DefaultGradleTask>) other.@tools.clone()
+
+        this.aerogearGcmMockCertificate = other.@aerogearGcmMockCertificate.copy()
+        this.aerogearGcmMockKey = other.@aerogearGcmMockKey.copy()
 
         // ports
 
@@ -165,90 +176,8 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
         turnProxyOn.resolve()
     }
 
-    // getters
-
-    public String getOpenShiftAppName() {
-        openShiftAppName.resolve()
-    }
-
-    public String getOpenShiftNamespace() {
-        openShiftNamespace.resolve()
-    }
-
-    public String getOpenShiftGearSize() {
-        openShiftGearSize.resolve()
-    }
-
-    public String getOpenShiftUsername() {
-        openShiftUsername.resolve()
-    }
-
-    public String getOpenShiftPassword() {
-        openShiftPassword.resolve()
-    }
-
-    public String getOpenShiftToken() {
-        openShiftToken.resolve()
-    }
-
     public CharSequence[] getOpenShiftCartridges() {
         openShiftCartridges.resolve().toArray(new CharSequence[0])
-    }
-
-    public Boolean getOpenShiftScale() {
-        openShiftScale.resolve()
-    }
-
-    public String getOpenShiftServer() {
-        openShiftServer.resolve()
-    }
-
-    public boolean getOpenShiftIgnoreIfExists() {
-        openShiftIgnoreIfExists.resolve()
-    }
-
-    public boolean getOpenShiftForce() {
-        openShiftForce.resolve()
-    }
-
-    public File getGitSsh() {
-        gitSsh.resolve()
-    }
-
-    public boolean getOpenShiftCheckout() {
-        openShiftCheckout.resolve()
-    }
-
-    public File getOpenShiftRepository() {
-        openShiftRepository.resolve()
-    }
-
-    public int getOpenShiftHttpProxyPort() {
-        openShiftHttpProxyPort.resolve()
-    }
-
-    public int getOpenShiftHttpsProxyPort() {
-        openShiftHttpsProxyPort.resolve()
-    }
-
-    public int getGcmMockServerPort() {
-        gcmMockServerPort.resolve()
-    }
-
-    public int getAerogearApnsPushPort() {
-        aerogearApnsPushPort.resolve()
-    }
-
-    public int getAerogearApnsFeedbackPort() {
-        aerogearApnsFeedbackPort.resolve()
-    }
-
-    public String getOpenShiftAppDirectory() {
-        openShiftAppDirectory.resolve()
-    }
-
-    public String getUnifiedPushTestExtensionVersion() {
-        unifiedPushTestExtensionVersion.resolve()
     }
 
     @Override
@@ -256,78 +185,79 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
 
         println 'Creating UPS OpenShift cartridge.'
 
-        File repositoryFile = null
-
-        File repository = getOpenShiftRepository()
-
+        File repository = openShiftRepository.resolve()
         if (repository == null || !repository.exists() || !repository.isDirectory()) {
-            repositoryFile = File.createTempDir()
-        } else {
-            repositoryFile = repository
+            repository = File.createTempDir()
         }
 
-        println "UPS OpenShift repository will be cloned to ${repositoryFile.canonicalPath}"
+        println "UPS OpenShift repository will be cloned to ${repository.canonicalPath}"
 
         Spacelift.task(CreateOpenShiftCartridge)
-                .server(getOpenShiftServer())
-                .named(getOpenShiftAppName())
-                .at(getOpenShiftNamespace())
-                .sized(getOpenShiftGearSize())
-                .username(getOpenShiftUsername())
-                .password(getOpenShiftPassword())
-                .token(getOpenShiftToken())
+                .server(openShiftServer.resolve())
+                .named(openShiftAppName.resolve())
+                .at(openShiftNamespace.resolve())
+                .sized(openShiftGearSize.resolve())
+                .username(openShiftUsername.resolve())
+                .password(openShiftPassword.resolve())
+                .token(openShiftToken.resolve())
                 .cartridges(getOpenShiftCartridges())
-                .scale(getOpenShiftScale())
-                .ignoreIfExists(getOpenShiftIgnoreIfExists())
-                .force(getOpenShiftForce())
-                .checkout(getOpenShiftCheckout())
-                .repo(repositoryFile.canonicalPath)
-                .gitSsh(getGitSsh())
+                .scale(openShiftScale.resolve())
+                .ignoreIfExists(openShiftIgnoreIfExists.resolve())
+                .force(openShiftForce.resolve())
+                .checkout(openShiftCheckout.resolve())
+                .repo(repository.canonicalPath)
+                .gitSsh(gitSsh.resolve())
                 .execute().await()
 
         ProcessResult ipGetResult = Spacelift.task(CommandTool).programName("rhc")
                 .parameters('ssh')
                 .parameters('--command', 'echo $OPENSHIFT_UNIFIED_PUSH_IP$OPENSHIFT_AEROGEAR_PUSH_IP$OPENSHIFT_JBOSS_UNIFIED_PUSH_IP$MYTESTIP_1')
-                .parameters('-a', getOpenShiftAppName())
-                .parameters('-n', getOpenShiftNamespace())
-                .parameters('-l', getOpenShiftUsername())
-                .parameters('-p', getOpenShiftPassword())
+                .parameters('-a', openShiftAppName.resolve())
+                .parameters('-n', openShiftNamespace.resolve())
+                .parameters('-l', openShiftUsername.resolve())
+                .parameters('-p', openShiftPassword.resolve())
                 .execute().await()
 
         String ip = ipGetResult.output().last()
 
+        def apnsCertificateTarget = new File(repository, "apns_server.p12")
+        def trustStoreTarget = new File(repository, "truststore.jks")
+
+        Spacelift.task(CertificateGenerator)
+            .apnsCertificate(apnsCertificateTarget)
+            .gcmCertificate(aerogearGcmMockCertificate.resolve())
+            .trustStore(trustStoreTarget)
+            .execute().await()
+
         if (turnProxyOn) {
+            String[] JAVA_OPTS_EXT_PARAMETERS = [
+                    "-Dhttp.proxyHost=$ip",
+                    "-Dhttp.proxyPort=${openShiftHttpProxyPort.resolve()}",
+                    "-Dhttps.proyHost=$ip",
+                    "-Dhttps.proxyPort=${openShiftHttpsProxyPort.resolve()}",
+                    "-Dgcm.mock.server.port=${gcmMockServerPort.resolve()}",
+                    "-Dcustom.aerogear.apns.push.host=$ip",
+                    "-Dcustom.aerogear.apns.push.port=${aerogearApnsPushPort.resolve()}",
+                    "-Dcustom.aerogear.apns.feedback.host=$ip",
+                    "-Dcustom.aerogear.apns.feedback.port=${aerogearApnsFeedbackPort.resolve()}",
+                    "-Djavax.net.ssl.trustStore=~/app-root/repo/aerogear.truststore",
+                    "-Djavax.net.ssl.trustStorePassword=aerogear" ]
 
-            StringBuilder sb = new StringBuilder()
-
-            String SPACE = " "
-
-            sb.append("JAVA_OPTS_EXT=")
-                    .append("-Dhttp.proxyHost=$ip").append(SPACE)
-                    .append("-Dhttp.proxyPort=" + getOpenShiftHttpProxyPort()).append(SPACE)
-                    .append("-Dhttps.proxyHost=$ip").append(SPACE)
-                    .append("-Dhttps.proxyPort=" + getOpenShiftHttpsProxyPort()).append(SPACE)
-                    .append("-Dgcm.mock.server.port=" + getGcmMockServerPort()).append(SPACE)
-                    .append("-Dcustom.aerogear.apns.push.host=$ip").append(SPACE)
-                    .append("-Dcustom.aerogear.apns.push.port=" + getAerogearApnsPushPort()).append(SPACE)
-                    .append("-Dcustom.aerogear.apns.feedback.host=$ip").append(SPACE)
-                    .append("-Dcustom.aerogear.apns.feedback.port=" + getAerogearApnsFeedbackPort()).append(SPACE)
-
-            String JAVA_OPTS_EXT = sb.toString()
+            String JAVA_OPTS_EXT = "JAVA_OPTS_EXT=${JAVA_OPTS_EXT_PARAMETERS.join(" ")}"
 
             println JAVA_OPTS_EXT
 
             Spacelift.task(CommandTool).programName("rhc")
                     .parameters('set-env', JAVA_OPTS_EXT)
-                    .parameters('-a', getOpenShiftAppName())
-                    .parameters('-n', getOpenShiftNamespace())
-                    .parameters('-l', getOpenShiftUsername())
-                    .parameters('-p', getOpenShiftPassword())
+                    .parameters('-a', openShiftAppName.resolve())
+                    .parameters('-n', openShiftNamespace.resolve())
+                    .parameters('-l', openShiftUsername.resolve())
+                    .parameters('-p', openShiftPassword.resolve())
                     .execute().await()
         }
 
-        def deployFile = "mv ~/app-root/repo/unifiedpush-test-extension-server.war ~/" + getOpenShiftAppDirectory() + "/standalone/deployments/unifiedpush-test-extension-server.war"
-        def actionHooksDir = new File(repositoryFile.canonicalPath, '.openshift/action_hooks/')
+        def deployFile = "mv ~/app-root/repo/unifiedpush-test-extension-server.war ~/" + openShiftAppDirectory.resolve() + "/standalone/deployments/unifiedpush-test-extension-server.war"
+        def actionHooksDir = new File(repository.canonicalPath, '.openshift/action_hooks/')
         actionHooksDir.mkdirs()
 
         List<File> addedFiles = Spacelift.task(WriteToFileTool)
@@ -335,26 +265,26 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
                 .to(new File(actionHooksDir, 'deploy'))
                 .execute().await()
 
-        String testExtensionWarPath = new StringBuilder()
-                .append(((File) parent['workspace']).canonicalPath)
-                .append("/.repository/org/jboss/aerogear/test/unifiedpush-test-extension-server/").append(getUnifiedPushTestExtensionVersion())
-                .append("/unifiedpush-test-extension-server-").append(getUnifiedPushTestExtensionVersion()).append(".war")
-                .toString()
+        String testExtensionWarPath = "${(parent['workspace'] as File).canonicalPath}" +
+                "/.repository/org/jboss/aerogear/test/unifiedpush-test-extension-server/${unifiedPushTestExtensionVersion.resolve()}" +
+                "/unifiedpush-test-extension-server-${unifiedPushTestExtensionVersion.resolve()}.war"
 
         def warFileSource = new File(testExtensionWarPath)
-        def warFileTarget = new File(repositoryFile, 'unifiedpush-test-extension-server.war')
+        def warFileTarget = new File(repository, 'unifiedpush-test-extension-server.war')
 
         Files.copy(warFileSource, warFileTarget)
 
-        Spacelift.task(repositoryFile, GitAddTool)
+        Spacelift.task(repository, GitAddTool)
                 .add(addedFiles)
                 .add(warFileTarget)
+                .add(apnsCertificateTarget)
+                .add(trustStoreTarget)
                 .execute().await()
 
         addedFiles.each { addedFile ->
             println addedFile.canonicalPath
             Spacelift.task(CommandTool)
-                    .workingDirectory(repositoryFile.canonicalPath)
+                    .workingDirectory(repository.canonicalPath)
                     .programName("git")
                     .parameters('update-index', '--chmod=+x', addedFile.canonicalPath)
                     .interaction(new ProcessInteractionBuilder().when(".*").printToOut())
@@ -365,15 +295,15 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
 
         println 'Committing unifiedpush-test-extension-server.war into the OpenShift repository.'
 
-        Spacelift.task(repositoryFile, GitCommitTool).message('Add test extension war.').execute().await()
+        Spacelift.task(repository, GitCommitTool).message('Add test extension war.').execute().await()
 
-        File gitSshFile = getGitSsh()
+        File gitSshFile = gitSsh.resolve()
 
         println 'Pushing added unifiedpush-test-extension-server.war into the OpenShift repository'
 
-        Spacelift.task(repositoryFile, GitPushTool).gitSsh(gitSshFile).execute().await()
+        Spacelift.task(repository, GitPushTool).gitSsh(gitSshFile).execute().await()
 
-        final String baseUri = "https://" + getOpenShiftAppName() + "-" + getOpenShiftNamespace() + ".rhcloud.com/unifiedpush-test-extension-server"
+        final String baseUri = "https://" + openShiftAppName.resolve() + "-" + openShiftNamespace.resolve() + ".rhcloud.com/unifiedpush-test-extension-server"
 
         println 'Waiting for unifiedpush-test-extension-server to be deployed. (max 5 minutes)'
         println "Expected deployment url: $baseUri"
@@ -418,10 +348,10 @@ class UPSOpenShiftInstallation extends BaseContainerizableObject<UPSOpenShiftIns
         Spacelift.task(CommandTool)
                 .programName("rhc")
                 .parameters('app', 'restart')
-                .parameters('-a', getOpenShiftAppName())
-                .parameters('-n', getOpenShiftNamespace())
-                .parameters('-l', getOpenShiftUsername())
-                .parameters('-p', getOpenShiftPassword())
+                .parameters('-a', openShiftAppName.resolve())
+                .parameters('-n', openShiftNamespace.resolve())
+                .parameters('-l', openShiftUsername.resolve())
+                .parameters('-p', openShiftPassword.resolve())
                 .execute().await()
 
         println 'Waiting for unifiedpush-test-extension-server to be deployed. (max 5 minutes)'
